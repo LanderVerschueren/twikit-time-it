a1fe859f09a3eefd0380547b38da5c0b8a346398
Lander Verschueren <lander@greenisland.be>
2021-11-10 19:28:42 +0100

    WIP: initial folders structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TimerComponent } from './timer/timer.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ListComponent } from './components/list/list.component';
+import { ListitemComponent } from './components/listitem/listitem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    ButtonComponent,
+    ListComponent,
+    ListitemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
